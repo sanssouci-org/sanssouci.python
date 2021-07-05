99542acb98cc6d256eb8a18175db0e1d4964d7f5
ablain <alexandre.blain@inria.fr>
2021-07-05 19:39:17 +0200

    refacto templates + add beta

--- sanssouci/reference_families.py
+++ sanssouci/reference_families.py
@@ -1,46 +1,11 @@
 import numpy as np
+from scipy.stats import beta
 
 
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # REFERENCE FAMILIES
 # R source code: https://github.com/pneuvial/sanssouci/
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-
-def t_inv_linear(p0):
-    """
-
-
-    Parameters
-    ----------
-
-    p0 : array-like of shape (B, p)
-        A numpy array of size [B,p] of null p-values obtained from
-        B permutations for p hypotheses.
-
-    Returns
-    -------
-
-    array-like of shape (B, p) :
-        numpy array with the same size of p0
-
-    Notes
-    -----
-
-    Warning: In case the last command does not work, you can use alternatively:
-
-    out=pval0_sorted_all*1.
-    for i in range(pval0_sorted_all.shape[0]):
-      out[i,:]=pval0_sorted_all[i,:]/normalized_ranks[:]
-    return out
-    """
-
-    p = p0.shape[1]
-
-    normalized_ranks = (np.arange(p) + 1) / float(p)
-
-    return p0 / normalized_ranks
-
 
 def t_linear(alpha, k, m):
     """
@@ -49,18 +14,74 @@ def t_linear(alpha, k, m):
     ----------
 
     alpha : float
-        A float in [0,1] as the confidence level
-    k : array-like of shape (K,)
-        sequence of integer frome 1 to K, could be generate with
-        np.arange(1, K+1) with K in [0,m]
+        confidence level in [0, 1]
+    k : int
+        number of rejection sets in linear template
     m : int
-        number of observations
+        number of hypotheses
 
     Returns
     -------
+    template : array of shape (k,)
+    """
+    return alpha * np.arange(1, k + 1) / m
 
-    array-like of shape (K,) :
 
+def t_beta(alpha, k, m):
+    """
+
+    Parameters
+    ----------
+
+    alpha : float
+        confidence level in [0, 1]
+    k : int
+        number of rejection sets in Beta template
+    m : int
+        number of hypotheses
+
+    Returns
+    -------
+    template : array of shape (k,)
+    """
+    return beta.ppf(alpha, np.arange(1, k + 1),
+                    np.array([m + 1] * k) - np.arange(1, k + 1))
+
+
+def t_inv_linear(y, k, m):
+    """
+    Parameters
+    ----------
+    y : array of floats
+        values to apply template to
+    k : int
+        index of rejection set in linear template
+    m : int
+        number of hypotheses
+    Returns
+    -------
+
+    estimated_template : array of same shape as y
 
     """
-    return alpha * k / (m * 1.0)
+
+    return y * m / k
+
+
+def t_inv_beta(y, k, m):
+    """
+    Parameters
+    ----------
+    y : array of floats
+        values to apply template to
+    k : int
+        index of rejection set in beta template
+    m : int
+        number of hypotheses
+    Returns
+    -------
+
+    estimated_template : array of same shape as y
+
+    """
+    return beta.cdf(y, k, m + 1 - k)
--- sanssouci/tests/test_reference_families.py
+++ sanssouci/tests/test_reference_families.py
@@ -1,22 +1,26 @@
 import numpy as np
-from sanssouci.reference_families import t_inv_linear, t_linear
+from sanssouci.reference_families import t_inv_linear, t_linear, t_beta
+from sanssouci.reference_families import t_inv_beta
+from scipy.stats import beta
 from numpy.testing import assert_array_almost_equal
 
 
 def test_t_inv_linear():
-    B = 50
     p = 20
     rng = np.random.RandomState(42)
-    p0 = np.sort(rng.rand(B, p), 1)
-    til = t_inv_linear(p0)
+    p0 = np.sort(rng.rand(p,))
+    til = t_inv_linear(p0, 2, p)
     assert (til >= p0).all()
     assert til.min() > 0.01
     assert til.max() < 10
     assert til.mean() > .9
-    assert til.mean() < 1.1
+    assert til.mean() < 5
 
     assert isinstance(til, np.ndarray)
     assert til.shape == p0.shape
+
+    til_full_template = t_inv_linear(p0, p, p)
+    assert_array_almost_equal(til_full_template, p0)
 
 
 def test_t_linear():
@@ -24,21 +28,32 @@ def test_t_linear():
     k = 5
     m = 10
     t = t_linear(alpha, k, m)
-    assert t == alpha * k / (m * 1.0)
-    assert isinstance(t, float)
-
-    alpha = .05
-    k = np.arange(1, 5)
-    m = 10
-    t = t_linear(alpha, k, m)
-    assert_array_almost_equal(t, alpha * k / (m * 1.0))
+    assert_array_almost_equal(t, alpha * np.arange(1, k + 1) / m)
     assert isinstance(t, np.ndarray)
-    assert len(t) == len(k)
+    assert len(t) == k
 
+
+def test_t_beta():
     alpha = .05
-    k = np.arange(1, 11)
+    k = 5
     m = 10
-    t = t_linear(alpha, k, m)
-    assert_array_almost_equal(t, alpha * k / (m * 1.0))
+    t = t_beta(alpha, k, m)
+    assert_array_almost_equal(t, beta.ppf(alpha, np.arange(1, k + 1),
+                              np.array([m + 1] * k) - np.arange(1, k + 1)))
     assert isinstance(t, np.ndarray)
-    assert len(t) == len(k)
+    assert len(t) == k
+
+
+def test_t_inv_beta():
+    p = 20
+    rng = np.random.RandomState(42)
+    p0 = np.sort(rng.rand(p,))
+    til = t_inv_beta(p0, 2, p)
+    assert (til >= p0).all()
+    assert til.min() > 0.01
+    assert til.max() <= 1
+    assert til.mean() > .5
+    assert til.mean() <= 1
+
+    assert isinstance(til, np.ndarray)
+    assert til.shape == p0.shape
