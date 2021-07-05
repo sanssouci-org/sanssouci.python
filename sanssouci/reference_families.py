8cbc1dd318299c7b48abe537ec6bdafb832e414b
ablain <alexandre.blain@inria.fr>
2021-07-05 19:42:57 +0200

    templates readability

--- sanssouci/reference_families.py
+++ sanssouci/reference_families.py
@@ -7,6 +7,26 @@ from scipy.stats import beta
 # R source code: https://github.com/pneuvial/sanssouci/
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
+def t_inv_linear(y, k, m):
+    """
+    Parameters
+    ----------
+    y : array of floats
+        values to apply template to
+    k : int
+        index of rejection set in linear template
+    m : int
+        number of hypotheses
+    Returns
+    -------
+
+    estimated_template : array of same shape as y
+
+    """
+
+    return y * m / k
+
+
 def t_linear(alpha, k, m):
     """
 
@@ -46,26 +66,6 @@ def t_beta(alpha, k, m):
     """
     return beta.ppf(alpha, np.arange(1, k + 1),
                     np.array([m + 1] * k) - np.arange(1, k + 1))
-
-
-def t_inv_linear(y, k, m):
-    """
-    Parameters
-    ----------
-    y : array of floats
-        values to apply template to
-    k : int
-        index of rejection set in linear template
-    m : int
-        number of hypotheses
-    Returns
-    -------
-
-    estimated_template : array of same shape as y
-
-    """
-
-    return y * m / k
 
 
 def t_inv_beta(y, k, m):
