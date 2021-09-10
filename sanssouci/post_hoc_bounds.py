23e34224291c7b217d5a1eb12c952177292142eb
Your Name <you@example.com>
2021-09-10 13:00:03 +0200

    test

--- sanssouci/post_hoc_bounds.py
+++ sanssouci/post_hoc_bounds.py
@@ -285,7 +285,7 @@ def find_largest_region(p_values, thresholds, TDP, masker=None):
     return region_size
 
 
-def compute_hommel_value(z_vals, alpha):
+def _compute_hommel_value(z_vals, alpha):
     """Compute the All-Resolution Inference hommel-value
     Function taken from nilearn.glm
     """
