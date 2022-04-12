d73885fc9b1ac1f6ef44252f0e3effa90fc98dc2
alexblnn <74506379+alexblnn@users.noreply.github.com>
2022-04-12 17:27:08 +0200

    Update setup.py
    add joblib + sklearn dependency to setup file + download link from release

--- setup.py
+++ setup.py
@@ -3,17 +3,18 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as readme_file:
     readme = readme_file.read()
 
-requirements = ["numpy>=1.15.0", "scipy>=1.0.0"]
+requirements = ["numpy>=1.15.0", "scipy>=1.0.0", "joblib>=1.0.1", "scikit-learn>=0.22"]
 
 setup(
     name="sanssouci",
-    version="0.1.2",
+    version="0.1.0",
     author="Laurent Risser and Pierre Neuvial",
     author_email="pierre.neuvial@math.univ-toulouse.fr",
     description="Post hoc inference via multiple testing",
     long_description=readme,
     long_description_content_type="text/markdown",
     url="https://github.com/sanssouci.python/",
+    download_url="https://github.com/pneuvial/sanssouci.python/archive/refs/tags/0.1.0.tar.gz",
     packages=find_packages(),
     install_requires=requirements,
     classifiers=[
