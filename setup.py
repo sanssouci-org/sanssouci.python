05231065d3c06ff15d99139f4fbba3af915ecdfa
Pierre Neuvial <pneuvial@users.noreply.github.com>
2025-10-16 22:53:39 +0200

    Update package version and author details

--- setup.py
+++ setup.py
@@ -12,18 +12,18 @@ requirements = [
 
 setup(
     name="sanssouci",
-    version="0.1.1",
-    author="Laurent Risser and Pierre Neuvial",
+    version="0.1.3",
+    author="Alexandre Blain, Nicolas Enjalbert Courrech, Pierre Neuvial, Nils Peyrouset, Laurent Risser, Bertrand Thirion",
     author_email="pierre.neuvial@math.univ-toulouse.fr",
     description="Post hoc inference via multiple testing",
     long_description=readme,
     long_description_content_type="text/markdown",
     url="https://github.com/pneuvial/sanssouci.python/",
-    download_url="https://github.com/pneuvial/sanssouci.python/archive/refs/tags/0.1.1.tar.gz",
+    download_url="https://github.com/pneuvial/sanssouci.python/archive/refs/tags/0.1.3.tar.gz",
     packages=find_packages(),
     install_requires=requirements,
     classifiers=[
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
     ],
 )
